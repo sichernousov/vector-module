bb3afa097ef0e92f25083dbc76a9e41a728ea1d6
Chernousov Stepan <stigo93@yandex.ru>
2018-10-25 17:38:44 +0300

    #3: main upd (added get_len)

--- main.c
+++ main.c
@@ -6,13 +6,12 @@
 int main ()
 {
 	vector3_t a = {-1, -1, -1};
- 	vector3_t b = { 2, -2,  2};
-	vector3_t c = { 0,  0,  0};
+ 	vector3_t b = { 2, -1,  2};
 
 	printf("VECTOR1 = (%d, %d, %d)\n", a.x, a.y, a.z);
 	printf("VECTOR2 = (%d, %d, %d)\n", b.x, b.y, b.z);
 
-	c = vector3_sum (&a, &b);
+	vector3_t c = vector3_sum (&a, &b);
 	printf("SUM = (%d, %d, %d)\n", c.x, c.y, c.z);
 
 	c = vector3_dif (&a, &b);
@@ -20,6 +19,9 @@ int main ()
 	
 	c = vector3_mult_num (&a, 5);
 	printf("MULT (v1, 5) = (%d, %d, %d)\n", c.x, c.y, c.z);
+
+	double len = vector3_get_len (&b);
+	printf("LEN (v2) = %.2f\n", len);
 
 	return 0;
 }
