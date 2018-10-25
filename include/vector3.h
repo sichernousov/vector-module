38dd04bf176f4836d117bec48175b9b89a16fa0d
Chernousov Stepan <stigo93@yandex.ru>
2018-10-25 17:37:40 +0300

    #3: added get_len oper

--- include/vector3.h
+++ include/vector3.h
@@ -9,5 +9,6 @@ typedef struct vector3_t
 vector3_t vector3_sum (vector3_t *v1, vector3_t *v2);
 vector3_t vector3_dif (vector3_t *v1, vector3_t *v2);
 vector3_t vector3_mult_num (vector3_t *v1, int num);
+double vector3_get_len (vector3_t *v1);
 
 #endif
--- lib/vector3.c
+++ lib/vector3.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 #include "../include/vector3.h"
+
+#define SQR(x) ((x)*(x))
 
 vector3_t vector3_sum (vector3_t *v1, vector3_t *v2)
 {
@@ -19,4 +22,9 @@ vector3_t vector3_mult_num (vector3_t *v1, int num)
 {
 	vector3_t tmp = { num*v1->x, num*v1->y, num*v1->z };
 	return tmp;
+};
+
+double vector3_get_len (vector3_t *v1)
+{
+	return sqrt( SQR(v1->x) + SQR(v1->y) + SQR(v1->z) ); 
 };
